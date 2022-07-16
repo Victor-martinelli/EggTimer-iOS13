5637b46ca811b5ef6eebd510b53cecb9856f29e8
Victor Martinelli <victormartinelli@Victors-MacBook-Pro-2.local>
2022-07-16 14:10:59 -0700

    WIP

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,19 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTimes = [
+        "Soft": 5,
+        "Medium": 7,
+        "Hard":12
+    ]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
-        if(hardness=="Soft"){
+        switch hardness {
+        case "Soft":
             print(softTime)
-        } else if(hardness=="Medium"){
+        case "Medium":
             print(mediumTime)
-        } else{
+        case "Hard":
             print(hardTime)
+        default:
+            print("Some other button got pressed")
         }
         
     }
